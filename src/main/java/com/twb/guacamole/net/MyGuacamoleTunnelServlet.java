d93595a774f75561fb31459ec5183cec8d69d023
Thomas Bigger <thomasbigger584@gmail.com>
2022-08-22 23:43:10 +0100

    Externalising parameters to js

--- src/main/java/com/twb/guacamole/net/MyGuacamoleTunnelServlet.java
+++ src/main/java/com/twb/guacamole/net/MyGuacamoleTunnelServlet.java
@@ -19,8 +19,6 @@
 
 package com.twb.guacamole.net;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.guacamole.GuacamoleException;
 import org.apache.guacamole.net.GuacamoleSocket;
 import org.apache.guacamole.net.GuacamoleTunnel;
@@ -30,24 +28,44 @@ import org.apache.guacamole.protocol.ConfiguredGuacamoleSocket;
 import org.apache.guacamole.protocol.GuacamoleConfiguration;
 import org.apache.guacamole.servlet.GuacamoleHTTPTunnelServlet;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.logging.Logger;
+
 /**
  * Simple tunnel example with hard-coded configuration parameters.
  */
 public class MyGuacamoleTunnelServlet extends GuacamoleHTTPTunnelServlet {
 
+    private final Logger logger = Logger.getLogger(MyGuacamoleTunnelServlet.class.getSimpleName());
+
+
+    // Connection Headers
+    private static final String HEADER_CONNECTION_PROTOCOL = "X-Connection-Protocol";
+    private static final String HEADER_CONNECTION_HOSTNAME = "X-Connection-Hostname";
+    private static final String HEADER_CONNECTION_PORT = "X-Connection-Port";
+    private static final String HEADER_CONNECTION_USER = "X-Connection-User";
+    private static final String HEADER_CONNECTION_PASSWORD = "X-Connection-Password";
+
+    //Guacd Headers
+    private static final String HEADER_GUACD_HOSTNAME = "X-Guacd-Hostname";
+    private static final String HEADER_GUACD_PORT = "X-Guacd-Port";
+
     @Override
     protected GuacamoleTunnel doConnect(HttpServletRequest request) throws GuacamoleException {
         // VNC connection information
+        // parameters found here: https://guacamole.apache.org/doc/0.8.3/gug/configuring-guacamole.html
         GuacamoleConfiguration config = new GuacamoleConfiguration();
-        config.setProtocol("vnc");
-        config.setParameter("hostname", "192.168.1.88");
-        config.setParameter("port", "5900");
-        config.setParameter("password", "password");
+        config.setProtocol(request.getHeader(HEADER_CONNECTION_PROTOCOL));
+        config.setParameter("hostname", request.getHeader(HEADER_CONNECTION_HOSTNAME));
+        config.setParameter("port", request.getHeader(HEADER_CONNECTION_PORT));
+        config.setParameter("username", request.getHeader(HEADER_CONNECTION_USER));
+        config.setParameter("password", request.getHeader(HEADER_CONNECTION_PASSWORD));
 
         // Connect to guacd, proxying a connection to the VNC server above
-        InetGuacamoleSocket inetGuacamoleSocket = new InetGuacamoleSocket("guacd", 4822);
+        String guacdHostname = request.getHeader(HEADER_GUACD_HOSTNAME);
+        int guacdPort = Integer.parseInt(request.getHeader(HEADER_GUACD_PORT));
+        InetGuacamoleSocket inetGuacamoleSocket = new InetGuacamoleSocket(guacdHostname, guacdPort);
         GuacamoleSocket socket = new ConfiguredGuacamoleSocket(inetGuacamoleSocket, config);
         return new SimpleGuacamoleTunnel(socket);
-
     }
 }
--- src/main/webapp/guacamole.js
+++ src/main/webapp/guacamole.js
@@ -2,7 +2,28 @@
 const display = document.getElementById("display");
 
 // Instantiate client, using an HTTP tunnel for communications.
-const tunnel = new Guacamole.HTTPTunnel("tunnel");
+const connectionProtocol = "vnc";
+const connectionHostname = "192.168.1.88";
+const connectionPort = "5900";
+const connectionUser = "";
+const connectionPassword = "password";
+const guacdHostname = "guacd";
+const guacdPort = "4822";
+
+const headers = {
+    "X-Connection-Protocol": connectionProtocol,
+    "X-Connection-Hostname": connectionHostname,
+    "X-Connection-Port": connectionPort,
+    "X-Connection-User": connectionUser,
+    "X-Connection-Password": connectionPassword,
+    "X-Guacd-Hostname": guacdHostname,
+    "X-Guacd-Port": guacdPort,
+};
+
+// Here is a more complete example of the tunnel usage.
+// https://github.com/apache/incubator-datalab/blob/4b1ec16844fefbf857071738ee94d6923fe4e447/services/self-service/src/main/resources/webapp/src/app/webterminal/webterminal.component.ts#L63
+// https://github.com/padarom/guacamole-common-js/blob/main/guacamole-common-js/src/main/webapp/modules/Tunnel.js#L251
+const tunnel = new Guacamole.HTTPTunnel("tunnel", false, headers);
 const guac = new Guacamole.Client(tunnel);
 
 // Add client to display div
